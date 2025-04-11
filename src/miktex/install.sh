4af65f0152cfaa9bb7c5fe59d60a942eb1d00819
Romain Ferrali <rferrali@gmail.com>
2025-04-11 12:19:18 +0000

    skip if miktex is installed

--- src/miktex/install.sh
+++ src/miktex/install.sh
@@ -6,15 +6,33 @@ if ! [ -f /etc/debian_version ]; then
     echo "Error: This script only supports Debian-based distributions."
     exit 1
 fi
-CURL_INSTALLED=1
-LSB_INSTALLED=1
-if ! command -v curl &> /dev/null; then
-    CURL_INSTALLED=0
-    apt-get update && apt-get install -y curl
+if command -v miktexsetup &> /dev/null; then
+    echo "MiKTeX is already installed. Skipping installation."
+    exit 0
 fi
-if ! command -v lsb_release &> /dev/null; then
+if command -v curl &> /dev/null; then
+    CURL_INSTALLED=1
+else
+    CURL_INSTALLED=0
+fi
+if command -v lsb_release &> /dev/null; then
+    LSB_INSTALLED=1
+else
     LSB_INSTALLED=0
-    apt-get update && apt-get install -y lsb-release
+fi
+if [ $CURL_INSTALLED -eq 0 -o $LSB_INSTALLED -eq 0 ]; then
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        apt-get update
+    fi
+    if [ $CURL_INSTALLED -eq 0 ]; then
+        apt-get update && apt-get install -y --no-install-recommends curl ca-certificates
+    fi
+    if [ $LSB_INSTALLED -eq 0 ]; then
+        apt-get update && apt-get install -y --no-install-recommends lsb-release
+    fi
+fi
+if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+    apt-get update
 fi
 curl -fsSL https://miktex.org/download/key | tee /usr/share/keyrings/miktex-keyring.asc > /dev/null
 SYSTEM_VERSION=$(lsb_release -cs)
@@ -25,7 +43,7 @@ else
     # Debian
     echo "deb [signed-by=/usr/share/keyrings/miktex-keyring.asc] https://miktex.org/download/debian $SYSTEM_VERSION universe" | tee /etc/apt/sources.list.d/miktex.list
 fi
-apt-get update && apt-get install -y miktex
+apt-get update && apt-get install -y --no-install-recommends miktex
 miktexsetup finish --shared
 initexmf --admin --set-config-value [MPM]AutoInstall=1
 if [ $CURL_INSTALLED -eq 0 ]; then
@@ -34,4 +52,8 @@ fi
 if [ $LSB_INSTALLED -eq 0 ]; then
     apt-get remove -y lsb-release
 fi
-apt-get clean && rm -rf /var/lib/apt/lists/*
+
+apt-get autoremove -y
+apt-get autoclean -y
+rm -rf /var/lib/apt/lists/*
+
