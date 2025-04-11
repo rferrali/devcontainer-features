1710118de18c8a1c4379ccc14e4f6254044a5507
Romain Ferrali <rferrali@gmail.com>
2025-04-11 11:35:04 +0000

    remove installation packages

--- src/miktex/install.sh
+++ src/miktex/install.sh
@@ -6,10 +6,14 @@ if ! [ -f /etc/debian_version ]; then
     echo "Error: This script only supports Debian-based distributions."
     exit 1
 fi
+CURL_INSTALLED=1
+LSB_INSTALLED=1
 if ! command -v curl &> /dev/null; then
+    CURL_INSTALLED=0
     apt-get update && apt-get install -y curl
 fi
 if ! command -v lsb_release &> /dev/null; then
+    LSB_INSTALLED=0
     apt-get update && apt-get install -y lsb-release
 fi
 curl -fsSL https://miktex.org/download/key | tee /usr/share/keyrings/miktex-keyring.asc > /dev/null
@@ -24,4 +28,10 @@ fi
 apt-get update && apt-get install -y miktex
 miktexsetup finish --shared
 initexmf --admin --set-config-value [MPM]AutoInstall=1
+if [ $CURL_INSTALLED -eq 0 ]; then
+    apt-get remove -y curl
+fi
+if [ $LSB_INSTALLED -eq 0 ]; then
+    apt-get remove -y lsb-release
+fi
 apt-get clean && rm -rf /var/lib/apt/lists/*
